9632ed949351322e88bc15963d4c92d853d56298
Yuqing Jin <91174109+yuqingjin@users.noreply.github.com>
2022-08-02 16:31:51 -0400

    Create 11.ContainerWithMostWater.py

---
+++ 11.ContainerWithMostWater.py
@@ -0,0 +1,22 @@
+# Method: Two-Pointer
+# Basic idea: shifting the smaller height side, bcs the amoount of water stored is determined by the smaller height side;
+# if we shifting the larger height, it is impossible to get more water
+# Time: O(N)
+# Space:O(1)
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l = 0
+        r = len(height)-1
+        max_water = 0
+        
+        while l < r:
+            cur_water = min(height[l], height[r]) * (r-l)
+            max_water = max(max_water, cur_water)
+            
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+                    
+        return max_water
