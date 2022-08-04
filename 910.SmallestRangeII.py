23d98dfdf1f49ede18cef89528586bc5e26adaac
Yuqing Jin <91174109+yuqingjin@users.noreply.github.com>
2022-08-03 20:02:42 -0400

    Create 910.SmallestRangeII.py

---
+++ 910.SmallestRangeII.py
@@ -0,0 +1,21 @@
+# Method: greedy algorithm
+# Basic Idea: deciding any two adjacent numbers smallest range, and compare it with the most left boundary and the most right boundary
+# Time: O(N)
+# Space: O(1)
+    
+class Solution:
+    def smallestRangeII(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        maxim, minim = nums[-1], nums[0]
+        
+        res = maxim - minim
+        up = maxim - k
+        down = minim + k 
+        
+        for i in range(1, len(nums)):
+
+            # 比较二者中更大的范围区间，和res的差异，取较小值
+            res = min(res, max(up, nums[i-1]+k)- min(down, nums[i]-k))
+        
+        return res
+        
