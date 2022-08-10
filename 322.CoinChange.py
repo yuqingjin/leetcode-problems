d076362cea6925a22597d78f0a6d826cc0b6178b
Yuqing Jin <91174109+yuqingjin@users.noreply.github.com>
2022-08-10 18:44:59 -0400

    Create 322.CoinChange.py

---
+++ 322.CoinChange.py
@@ -0,0 +1,26 @@
+# Method: DP
+# T: O(12*N)
+# S: O(N)
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        # 这个写在下面的那个前面
+        if amount == 0:
+            return 0
+        
+        # when there is no way to break up the amount
+        if amount < min(coins):
+            return -1
+        
+        coins.sort()
+        dp = [inf] * (amount+1)
+        dp[0] = 0
+        
+        for i in range(1, amount+1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], dp[i-coin]+1)
+        
+        return dp[-1] if dp[-1] != inf else -1
+        
