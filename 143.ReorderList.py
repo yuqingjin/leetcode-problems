5310ae8e2115854e42b893a1ef3d2aaa19adea05
Yuqing Jin <91174109+yuqingjin@users.noreply.github.com>
2022-08-14 19:05:41 -0400

    Create 143.ReorderList.py

---
+++ 143.ReorderList.py
@@ -0,0 +1,38 @@
+# Method: hashmap, linked list, left/right pointers
+# T: O(N)
+# S: O(N)/O(1) for slow/fast pointers method
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        
+        hashmap = []
+        snipper = head
+        
+        while snipper != None:
+            hashmap.append(snipper)
+            snipper = snipper.next
+        
+        
+        l, r = 0, len(hashmap) - 1
+        dummy = ListNode()
+        while l <= r:
+            # print(l, r)
+            dummy.next = hashmap[l]
+            dummy = dummy.next
+            # break the link firstly, so that there would be no cycle in list!
+            dummy.next = None
+            l += 1
+            
+            # print(l, r)  
+            dummy.next = hashmap[r]
+            dummy = dummy.next
+            dummy.next = None
+            r -= 1
