643145842f16f6cd1108e221bc883dd148b0da3f
Yuqing Jin <91174109+yuqingjin@users.noreply.github.com>
2022-08-27 21:17:41 -0400

    Create 518.CoinChange2.py

---
+++ 518.CoinChange2.py
@@ -0,0 +1,17 @@
+# Method: dp, 01背包
+# 求所有组合数，所以遍历顺序为：外层遍历物品，内层遍历背包
+# T: O(m*n), m-len(coins), n-amount
+# S: O(n)
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount+1)
+        # 凑成面额为0的方法有1种
+        dp[0] = 1
+        
+        # 求组合数，所以外层遍历物品，内层遍历背包
+        for coin in coins:
+            for i in range(coin, amount+1):
+                    dp[i] += dp[i-coin]
+                    
+        return dp[-1]
