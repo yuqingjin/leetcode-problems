4510452add9a774cc6fdcf77b8f2ca3ed03131ed
Yuqing Jin <91174109+yuqingjin@users.noreply.github.com>
2023-03-01 20:02:46 -0500

    Update 76.MinimumWindowSubstring.py

--- 76.MinimumWindowSubstring.py
+++ 76.MinimumWindowSubstring.py
@@ -2,35 +2,32 @@
 # Time: O(S+T);  S-length of s, T-length of t
 # Space: O(26)
 
+from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        
-        t_dict = collections.Counter(t)
-        # print(t_dict)
-        t_length = len(t_dict)
+        dict_t = Counter(t)
+        dict_s = Counter(s)
         match = 0
-        min_length = inf
-        res = ''
-        
+        min_length = float("inf")
+        t_length = len(dict_t)
+
+        res = ""
         l = 0
-        m, n = len(t), len(s)
-        
-        if m > n:
-            return ''
-        
+
+        # expand right boundary
         for r in range(len(s)):
-            t_dict[s[r]] -= 1
-        
-            if t_dict[s[r]] == 0:
-                match +=1
-                
-            while match == t_length: 
+            dict_t[s[r]] -= 1
+            if dict_t[s[r]] == 0:
+                match += 1
+            
+            # moving the left boundary
+            while match == t_length:
                 if min_length > r-l+1:
                     res = s[l:r+1]
                     min_length = r-l+1
-                t_dict[s[l]] += 1
-                if t_dict[s[l]] > 0:
+                dict_t[s[l]] += 1
+                if dict_t[s[l]] > 0:
                     match -= 1
-                l+=1
-            
+                l += 1
+
         return res
